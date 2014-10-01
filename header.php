cbe7222b5ceb0769c44f1105a3e2dc274f27c94c
anbuf <anthony@selectivefew.com>
2014-09-30 23:25:34 -0400

    Add basic content

---
+++ header.php
@@ -0,0 +1,18 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="index,follow" />
+	<link rel="shortcut icon" href="http://www.aep.polymtl.ca/favicon.ico" />
+    <title>Stationnement - AEP</title>
+
+    <link rel="stylesheet" type="text/css" href="CSS/style.css?<?php echo time(); ?>" />
+    </script>
+    <script type="text/javascript" src="http://www.aep.polymtl.ca/index.php?title=-&amp;action=raw&amp;gen=js"></script>
+</head>
+
+	<body>
+    <div id="main">
+        <div id="banner">
+            <div id="topleft"><a href="index.php"><img src="images/BanniereStationnement.png" alt="AEP Logo" id="aeplogo"/></a></div>
+        </div>
+    
